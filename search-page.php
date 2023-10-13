ed2862c471e57c201256a0f8ad44546ab1975d04
KrithikJ <kjais488@mtroyal.ca>
2023-10-12 20:09:28 -0600

    Started the styling of all pages with css and bootstrap.

--- search-page.php
+++ search-page.php
@@ -10,8 +10,10 @@ include 'includes/functions.inc.php';
 <!DOCTYPE html>
 
 <head>
-
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/search-pagecss.css">
 
 </head>
 
@@ -22,37 +24,68 @@ include 'includes/functions.inc.php';
 <header>
 
 <!--nav bar goes here -->
+    <h1>Krithik and Paras's Song Database</h1>
+    <h2>Song Search</h2>
 
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navigate</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#">Home</a> <!-- Need to add link to home page once its creates -->
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="./search-page.php#">Song Search</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./single-song-page.php#">Song Info</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="./search-results-page.php#">Search Results</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./view-favorites-page.php#">View Favorites</a>
+        </li>
+      </form>
+    </div>
+  </div>
+</nav>
 </header>
 
 
 <main>
 
 
-<!-- the form that the user will interact with -->
 
-<form action = "./search-results-page.php" method = "GET">
+<!-- the form that the user will interact with -->
+<fieldset>
+  <form action = "./search-results-page.php" method = "GET">
 
 
 <!-- Title section -->
 
-
-<input type ='radio' name = 'type[]' value = 't'> 
-Title
-<input type = "text" name = 'titleName'>
+<div class="form-check">
+    <input class="form-check-input" type="radio" name='type[]' value='t' id="flexRadioDefault1">
+    <div class="input-group mb-3">
+    <span class="input-group-text" id="inputGroup-sizing-default">Title</span>
+    <input type="text" name = 'titleName' class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+</div>
 
 </br>
 
 <!-- Artist section -->
-
-<input type ='radio' name = 'type[]' value = 'a'> 
-Artist
-
-
-<select name = 'artistName'>
-
-<option></option>
-
+<div class="input-group mb-3">
+<div class="form-check">
+    <input class="form-check-input" type="radio" name='type[]' value='a' id="flexRadioDefault1">
+    <div class="input-group mb-3">
+  <label class="input-group-text" for="inputGroupSelect01">Artist</label>
+  <select class="form-select" name='artistName' id="inputGroupSelect01">
+    <option></option>
 
 <?php
 
@@ -75,24 +108,23 @@ foreach($data as $d)
 } 
 
 ?>
-  
-
 </select>
 
-
 </br>
+</div>
+</div>
+</div>
 
 <!-- genre section -->
+<div class="input-group mb-3">
+<div class="form-check">
+    <input class="form-check-input" type="radio" name='type[]' value='g' id="flexRadioDefault1">
+    <div class="input-group mb-3">
+  <label class="input-group-text" for="inputGroupSelect01">Genre</label>
+  <select class="form-select" name='genreName' id="inputGroupSelect01">
+    <option></option>
 
-<input type ='radio' name = 'type[]' value = 'g'> 
-Genre
-
-<select name = 'genreName'>
-
-<option></option>
-
-
-<?php
+    <?php
 
 
 $genresGateway = new GenresDB($conn);
@@ -114,32 +146,48 @@ foreach($data as $d)
 </select>
 
 </br>
-
+</div>
+</div>
+</div>
 
 <!-- year section -->
 
-<input type ='radio' name = 'type[]' value = 'y'> 
+<div class="form-check">
+<input class="form-check-input" type="radio" name='type[]' value='y' id="flexRadioDefault1">
 Year
+<br>
+<br>
 
-<input type ='radio' name = 'lessOrGreater[]' value = 'less'> 
-Less
-<input type = 'text' name = 'lessThanYearValue'>
+<div class="form-check">
+    <input class="form-check-input" type="radio" name='lessOrGreater[]' value='less' id="flexRadioDefault1">
+    <div class="input-group mb-3">
+    <span class="input-group-text" id="inputGroup-sizing-default">Less</span>
+    <input type="text" name = 'lessThanYearValue' class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+</div>
 
-<input type ='radio' name = 'lessOrGreater[]' value = 'greater'> 
-Greater
-<input type = 'text' name = 'greaterThanYearValue'>
+<div class="form-check">
+    <input class="form-check-input" type="radio" name='lessOrGreater[]' value='greater' id="flexRadioDefault1">
+    <div class="input-group mb-3">
+    <span class="input-group-text" id="inputGroup-sizing-default">Greater</span>
+    <input type="text" name = 'greaterThanYearValue' class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+</div>
+</div>
 
 </br>
 
 <input type = 'submit'>
 
-</form>
+</form>  
+</fieldset>
+
 
 
 </main>
 
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 
--- search-results-page.php
+++ search-results-page.php
@@ -1,3 +1,50 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/search-pagecss.css">
+
+</head>
+
+<body>
+    
+
+<header>
+
+<!--nav bar goes here -->
+    <h1>Krithik and Paras's Song Database</h1>
+    <h2>Search Results</h2>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navigate</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#">Home</a> <!-- Need to add link to home page once its creates -->
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./search-page.php#">Song Search</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./single-song-page.php#">Song Info</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="./search-results-page.php#">Search Results</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./view-favorites-page.php#">View Favorites</a>
+        </li>
+      </form>
+    </div>
+  </div>
+</nav>
+</header>
+
 <?php
 
 
@@ -358,3 +405,5 @@ die($e->getMessage());
 }
 
 ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
